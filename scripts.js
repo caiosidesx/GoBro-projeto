49b73d1ea4b04edd1443577efb639124db0df5df
caiosidesx <118694590+caiosidesx@users.noreply.github.com>
2025-09-03 17:03:48 -0300

    Atualização 10
    Adicionado o @media query, para responsividade, e Dando início ao JAVASCRIPT:  Direcionar o usuário ao Whatsapp, CONCLUÍDO.

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// USUÁRIO IRÁ CLICAR NO BOTÃO E SERÁ DIRECIONADO AO WHATSAPP DA LOJA //
+/*
+    - COMO DIRECIONAR O USUÁRIO AO WHATSAPP (PASSO A PASSO)
+    1 - CRIAR UMA FUNÇÃO COM O NOME "abrirWhatsapp()"
+    2 - DENTRO DESSA FUNÇÃO, PEGO OS DADOS QUE PRECISO E GUARDO ESSAS INFORMAÇÕES DENTRO DE UMA VARIÁVEL
+    - let numero = number (numero da loja)
+    - let mensagem = "string" (Mensagem de início de conversa)
+    - let url = "https://wa.me/" + numero + "?text=" + encodeURIComponent(mensagem)
+    3 - PARA RODAR O COMANDO DA FUNÇÃO BASTA: 
+        - window.open(url, "_blank")
+*/
+function abrirWhatsapp() {
+    let numero = 5579998524174
+    let mensagem = "Olá, quero saber mais sobre o atendimento de vocês!"
+    let url = "https://wa.me/" + numero + "?text=" + encodeURIComponent(mensagem)
+
+    window.open(url, "_blank")
+}
+/*PEGAR OS DADOS DO USUÁRIO (INSCRIÇÃO PARA VAGA NA LOJA) VALIDAÇÕES DE USUÁRIO*/ 
